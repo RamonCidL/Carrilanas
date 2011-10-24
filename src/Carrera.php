51e959f61fcb3ccbb421021243292f9b6cc98595
RamonCidL <ramon.cid@gmail.com>
2011-10-24 12:32:53 +0200

    imagenes

--- src/Carrera.php
+++ src/Carrera.php
@@ -18,6 +18,51 @@ class Carrera extends MasterTable{
 		$this->level = 1;
 		parent::__construct();
 	}
+	function getTable(){
+		$ret = '{' ;
+		if($_SESSION['nivel'] > 5){
+			$ret .= ' "add"      : "true"';
+			$ret .= ',"edit"     : "true"';
+			$ret .= ',"delete"   : "true"';
+		}else{
+			$ret .= ' "add"      : "false"';
+			$ret .= ',"edit"     : "false"';
+			$ret .= ',"delete"   : "false"';
+		}
+		$ret .= ',
+			"colModel" : [
+				
+				{"display": "lugar",            "name" : "lugar",           "width" : 150 },
+				{"display": "Fecha",            "name" : "fecha",           "width" : 250 },
+				{"display": "Distancia",        "name" : "distancia",       "width" : 250 },
+				{"display": "MapaRecorrido",    "name" : "Maparecorrido",   "width" : 250 },
+				{"display": "ComoLlegar",       "name" : "comollegar",      "width" : 250 },
+				{"display": "PrediccionTiempo", "name" : "predicciontiempo","width" : 250 },
+				{"display": "Comentario",       "name" : "comentario",      "width" : 250 }
+			]
+		}';
+		return $ret;
+	}
+	function getForm(){
+		return '{
+			"colModel" : [
+				{"type":"text",  "display": "Lugar",           "value" : "lugar",   		  "width" : 150 },
+				{"type":"date",  "display": "Fecha",    	   "value" : "fecha",             "width" : 250 },
+				{"type":"text",  "display": "Distancia",       "value" : "distancia",    	  "width" : 25 },
+				{"type":"image", "display": "Mapareccorido",   "value" : "maparecorrido",     "width" : 25 }
+				{"type":"text",  "display": "MComoLlegar",     "value" : "comollegar",   	  "width" : 25 },
+				{"type":"text",  "display": "PrediccionTiempo","value" : "predicciontiempo",  "width" : 25 },
+				{
+					"type"     : "textarea"     , 
+					"display"  : "Comentarios"  ,     
+					"value"    : "comentario"   ,     
+					"width"    : 50             , 
+					"height"   : 2 
+				},
+				
+			]
+		}';
+	}
 
 	function isValidForm($formvars) {
 		$this->error = null;
